133a8b6f5ed8c7efb9cc57bf5dc1f65b6e6a7267
maymuna987 <159554212+maymuna987@users.noreply.github.com>
2024-12-02 01:17:12 +0600

    admin delete product

--- admin_area/delete_pro.php
+++ admin_area/delete_pro.php
@@ -1,5 +1,23 @@
 <?php
 include('../admin_area/header.php');
 include('../include/db.php');
+
+if(isset($_GET['delete_pro'])){
+    $delete_id=$_GET['delete_pro'];
+
+    $delete_product="Delete from products where pro_id= $delete_id";
+    $result=mysqli_query($con,$delete_product);
+    if($result){
+        echo "<script>alert('Product deleted successfully')</script>";
+        echo "<script>window.open('./index.php','_self')</script>";
+
+    }
+    
+}
+
+?>
+
+
+<?php
 include('footer.php');
 ?>
